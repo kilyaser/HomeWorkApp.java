55fb0b82738f1a6b3f68b0a93b9d12e488b36834
Leva <piknik0711@yandex.ru>
2022-01-28 17:47:41 +0300

    added task 3

--- src/main/java/ru/geekbrains/lesson3/HomeWorkApp_three.java
+++ src/main/java/ru/geekbrains/lesson3/HomeWorkApp_three.java
@@ -4,17 +4,21 @@ import java.util.Arrays;
 
 public class HomeWorkApp_three {
     public static void main(String[] args) {
-        //1
+        //to check decision 1
         int[] input = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         System.out.println(Arrays.toString(booleanNegateArray(input)));
 
-        //2
+        //to check decision 2
         int[] massive = fillUpArray(100);
-        System.out.print(Arrays.toString(massive));
+        System.out.println(Arrays.toString(massive));
+
+        // to check decision 3
+        int[] m = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println(Arrays.toString(doubleIfLess(m)));
 
     }
 
-    //1
+    //task 1
     public static int[] booleanNegateArray(final int[] input){
         for (int i = 0; i < input.length; i++){
             if(input[i] == 0){
@@ -26,13 +30,23 @@ public class HomeWorkApp_three {
         return input;
     }
 
-    //2
+    //task 2
     public static int[] fillUpArray(final int length){
         int[] result = new int[length];
         for(int i = 1; i <= result.length; i++){
             result[i-1] = i;
         }
         return result;
+    }
+
+    //task 3
+    public static int[] doubleIfLess(final int[] input){
+        for(int i = 0; i < input.length; i++){
+            if(input[i] < 6){
+                input[i] *= 2;
+            }
+        }
+        return input;
     }
 
 }
