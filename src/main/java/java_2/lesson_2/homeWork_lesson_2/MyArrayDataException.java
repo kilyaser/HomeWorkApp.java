f24ea4582d783dcb04aa33081ecad2fe90c9de1f
Leva <piknik0711@yandex.ru>
2022-04-17 16:52:09 +0300

    added homework lesson 2 (tasks 1 - 3)

---
+++ src/main/java/java_2/lesson_2/homeWork_lesson_2/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package java_2.lesson_2.homeWork_lesson_2;
+
+public class MyArrayDataException extends Exception{
+    public MyArrayDataException(String exception) {
+        System.out.println(exception);
+    }
+}
---
+++ src/main/java/java_2/lesson_2/homeWork_lesson_2/MyArraySizeException.java
@@ -0,0 +1,4 @@
+package java_2.lesson_2.homeWork_lesson_2;
+
+public class MyArraySizeException extends Exception {
+}
